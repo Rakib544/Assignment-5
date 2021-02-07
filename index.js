c19914fc8f6c762873e17de71c42e378a12c83fd
Rakib544 <md.rakib10122003@gmail.com>
2021-02-07 09:51:28 +0600

    half done

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const showFood = foods => {
         notFoundMessage.classList.add('mx-auto', 'fs-2')
         document.getElementById('parent-div').appendChild(notFoundMessage);
     }
-} 
+}
 
 
 //handling selected food item's full information functionality 
@@ -50,5 +50,15 @@ const showFullDetails = (mealId) => {
         .then(res => res.json())
         .then(data => {
             console.log(data);
+            const fullDetailsDiv = document.getElementById('full-details');
+            fullDetailsDiv.innerHTML = `
+                    <div class="card">
+                        <img src=${data.meals[0].strMealThumb} class="card-img-top height" alt="...">
+                        <div class="card-body">
+                            <h4 class="card-title">${data.meals[0].strMeal}</h4>
+                            <p>✅ </p>
+                        </div>
+                    </div>
+            `
         })
 }
